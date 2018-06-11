ef53ad6175368c9cbd582d0c3f4ddf527f348eb2
Henry Zhuang <henry@mingtak.com.tw>
2018-06-11 14:00:49 +0800

    update interfaces.py

--- src/sayka/content/interfaces.py
+++ src/sayka/content/interfaces.py
@@ -7,6 +7,7 @@ from plone.app.textfield import RichText
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.supermodel.directives import fieldset
 
 
 class ISaykaContentLayer(IDefaultBrowserLayer):
@@ -143,70 +144,126 @@ class ICover(Interface):
         title=_(u'Title'),
         required=True
     )
+
     banner1_title = schema.TextLine(
         title=_(u'Banner1 Title'),
         required=True
     )
-    description1 = schema.Text( 
+    description1 = RichText(
         title=_(u"Dsecription1"),
-        required=True
-    )
-    banner1 = NamedBlobImage(
-        title=_(u"Banner Image."),
         required=False,
     )
+    banner1 = NamedBlobImage(
+        title=_(u"Banner1 Image."),
+        required=False,
+    )
+
     banner2_title = schema.TextLine(
         title=_(u'Banner2 Title'),
         required=True
     )
-    description2 = schema.Text( 
+    description2 = RichText(
         title=_(u"Dsecription2"),
-        required=True
+        required=False
     )
     banner2 = NamedBlobImage(
         title=_(u"Banner2 Image."),
         required=False,
     )
+
     banner3_title = schema.TextLine(
         title=_(u'Banner3 Title'),
         required=True
     )
-    description3 = schema.Text( 
+    description3 = RichText(
         title=_(u"Dsecription3"),
-        required=True
+        required=False,
     )
     banner3 = NamedBlobImage(
         title=_(u"Banner3 Image."),
         description=_(u"Banner image."),
         required=False,
     )
+
     banner4_title = schema.TextLine(
         title=_(u'Banner4 Title'),
         required=True
     )
-    description4 = schema.Text( 
+    description4 = RichText(
         title=_(u"Dsecription4"),
-        required=True
+        required=False,
     )
     banner4 = NamedBlobImage(
-        title=_(u"Banner Image."),
+        title=_(u"Banner4 Image."),
         description=_(u"Banner image."),
         required=False,
     )
-    description4 = schema.Text( 
-        title=_(u"Dsecription4"),
-        required=True
-    )
+
     banner5_title = schema.TextLine(
         title=_(u'Banner5 Title'),
         required=True
     )
-    description5 = schema.Text( 
+    description5 = RichText(
         title=_(u"Dsecription5"),
-        required=True
+        required=False
     )
     banner5 = NamedBlobImage(
-        title=_(u"Banner Image."),
+        title=_(u"Banner5 Image."),
         description=_(u"Banner image."),
         required=False,
     )
+
+    fieldset(_(u'Cover Background Images'),
+             fields=['bg_1', 'bg_2'])
+    bg_1 = NamedBlobImage(
+        title=_(u'Background image 1'),
+        required=True,
+    )
+    bg_2 = NamedBlobImage(
+        title=_(u'Background image 2'),
+        required=True,
+    )
+
+    fieldset(_(u'Cover Footer Slider'),
+             fields=['f_slider_1', 'f_slider_desc_1',
+                     'f_slider_2', 'f_slider_desc_2',
+                     'f_slider_3', 'f_slider_desc_3',
+                     'f_slider_4', 'f_slider_desc_4',])
+    f_slider_1 = NamedBlobImage(
+        title=_(u"Footer Slider."),
+        required=True,
+    )
+    f_slider_desc_1 = schema.TextLine(
+        title=_(u'Footer Slider Description'),
+        required=True
+    )
+
+    f_slider_2 = NamedBlobImage(
+        title=_(u"Footer Slider."),
+        required=True,
+    )
+    f_slider_desc_2 = schema.TextLine(
+        title=_(u'Footer Slider Description'),
+        required=True
+    )
+
+    f_slider_3 = NamedBlobImage(
+        title=_(u"Footer Slider."),
+        required=True,
+    )
+    f_slider_desc_3 = schema.TextLine(
+        title=_(u'Footer Slider Description'),
+        required=True
+    )
+
+    f_slider_4 = NamedBlobImage(
+        title=_(u"Footer Slider."),
+        required=True,
+    )
+    f_slider_desc_4 = schema.TextLine(
+        title=_(u'Footer Slider Description'),
+        required=True
+    )
+
+
+
